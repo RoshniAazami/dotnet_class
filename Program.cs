adf79adb6071d587707f63f1ccfb5de3f2c70a4a
RoshniAazami <roshniiaazamii@gmail.com>
2026-06-23 20:42:13 +0545

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System; //overloading constructor
+class Cons
+{
+    private int sum;
+    public Cons (int a, int b)
+    {
+        sum=a+b;
+        Console.WriteLine("sum={0}", sum);
+    }
+    public Cons(int a, int b, int c)
+    {
+        sum=a+b+c;
+        Console.WriteLine("sum={0}",sum);
+    }
+}
+class Program
+{
+    static void Main(String[]args)
+    {
+        new Cons(10,20);
+        new Cons(10,20,30);
+    }
+}
+//output
+//sum=30
+//sum=60
